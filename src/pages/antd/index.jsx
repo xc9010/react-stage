3344868e294ec791112e54fc29480d9a1475536a
xc9010 <xuchen199010@gmail.com>
2020-12-25 17:47:07 +0800

    add-eslint

--- src/pages/antd/index.jsx
+++ src/pages/antd/index.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { Button } from 'antd';
 import styles from '../../index.css'
 
-const Antd = (props) => {
-  console.log('props', props)
-  return (
-    <div className={styles.main}>
-      <h2>button</h2>
-      <Button type="primary">Primary Button</Button>
-    </div>
-  )
-}
+const Antd = () => (
+  <div className={styles.main}>
+    <h2>button</h2>
+    <Button type="primary">Primary Button</Button>
+  </div>
+);
 
 export default Antd;
